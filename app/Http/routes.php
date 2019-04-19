f0dc63a95f2e3c762ed31eb5af7fdf2adc43e6e4
mohamedmahmoudf97 <43675287+mohamedmahmoudf97@users.noreply.github.com>
2019-04-19 20:35:53 +0300

    admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+Route::resource('/admin/user' , 'AdminUserController');
+Route::get('/admin', function () {
+    return view('admin.index');
+});
